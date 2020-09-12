10702603b6ee99175391ccef3dcd846b94d272c2
Szymanek93 <59121890+Szymanek93@users.noreply.github.com>
2020-09-12 21:07:32 +0200

    - CRUD ready

--- src/main/java/com/szymansky/SBM/RestController/EmployeeRestController.java
+++ src/main/java/com/szymansky/SBM/RestController/EmployeeRestController.java
@@ -1,17 +1,26 @@
 package com.szymansky.SBM.RestController;
 
 
+import com.sun.el.lang.ELArithmetic;
 import com.szymansky.SBM.Entity.Employee;
+import com.szymansky.SBM.Exception.ResourceNotFoundException;
 import com.szymansky.SBM.Repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.ws.Response;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/employee")
 @AllArgsConstructor
@@ -22,61 +31,67 @@ public class EmployeeRestController {
     private final EmployeeRepository emlpoyeRepo;
 
 
-
     @GetMapping("/{id}")
-    public Employee getEmployees(@PathVariable Long id) {
-        return employeeList.stream()
-                .filter(employee -> employee.getId() == id).findFirst().get();
-    }
-    //wyszukiwanie wszystkich obiektów
-    @GetMapping("/findAll")
-    public List<Employee> getEmployees() {
-        List<Employee> employeeList=emlpoyeRepo.findAll();
-        return employeeList;
+    public ResponseEntity<Employee> getEmployeesById(@PathVariable(value = "id") Long id)
+            throws ResourceNotFoundException {
+        Employee employee = emlpoyeRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + id));
+        return ResponseEntity.ok().body(employee);
     }
 
-    //dodawanie nowego obiektu
-    @ResponseBody
+    //wyszukiwanie wszystkich obiektów
+    @GetMapping("")
+    public List<Employee> getEmployees() {
+        return emlpoyeRepo.findAll();
+    }
+
+    //add new record
     @PostMapping("/add")
     public Employee addEmployee(@RequestBody Employee employee) {
-        Optional<Employee> employee1 = emlpoyeRepo.findById((employee.getId()));
-        if (employee1.isPresent()!=true){
-            return emlpoyeRepo.save(employee);
-        }
-        else
-            return null;
-
+        return emlpoyeRepo.save(employee);
     }
 
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+  public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long id, @RequestBody Employee employeeDettails)
+        throws ResourceNotFoundException{ Employee employee = emlpoyeRepo.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + id));
+
+        employee.setId(employeeDettails.getId());
+        employee.setName(employeeDettails.getName());
+        employee.setLastName(employeeDettails.getLastName());
+        final Employee updatedEmployee = emlpoyeRepo.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
 
     @DeleteMapping("/{id}")
-    public void deleteEmployee(@PathVariable Long id){
-        getEmployees(id);
-        emlpoyeRepo.deleteById(id);
-        System.out.print("Pomyślnie usunięto");
-        System.out.println(id);
-        getEmployees();
+    public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") Long id)
+            throws ResourceNotFoundException {
+        Employee employee = emlpoyeRepo.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id::" + id));
+
+        emlpoyeRepo.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 
-    @EventListener(ApplicationReadyEvent.class)// wykonuje sie przyuruchomieniu aplikacji
-    //@PostMapping("/dodaj")
-    public void InitializeEmployees() {
-        System.out.println("Adding employees");
-        Employee employee1 = new Employee("Jan", "Borys");
-        Employee employee2 = new Employee( "Kazimierz", "Jan");
-        Employee employee3 = new Employee( "Borys", "Kazimierz");
-        Employee employee4 = new Employee( "Bys", "ierz");
-
-        emlpoyeRepo.save(employee1);
-        emlpoyeRepo.save(employee2);
-        emlpoyeRepo.save(employee3);
-        emlpoyeRepo.save(employee4);
-        employeeList.add(employee1);
-        employeeList.add(employee2);
-        employeeList.add(employee3);
-        employeeList.add(employee4);
-        System.out.println("dodano");
-        System.out.println(employeeList);
-    }
+//    @EventListener(ApplicationReadyEvent.class)// wykonuje sie przyuruchomieniu aplikacji
+//    //@PostMapping("/dodaj")
+//    public void InitializeEmployees() {
+//        Employee employee1 = new Employee("Jan", "Borys");
+//        Employee employee2 = new Employee("Kazimierz", "Jan");
+//        Employee employee3 = new Employee("Borys", "Kazimierz");
+//        Employee employee4 = new Employee("Bys", "ierz");
+//        emlpoyeRepo.save(employee1);
+//        emlpoyeRepo.save(employee2);
+//        emlpoyeRepo.save(employee3);
+//        emlpoyeRepo.save(employee4);
+//        employeeList.add(employee1);
+//        employeeList.add(employee2);
+//        employeeList.add(employee3);
+//        employeeList.add(employee4);
+//        System.out.println(employeeList);
+//    }
 }
 
